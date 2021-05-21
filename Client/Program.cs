1c01d1f36a2df528c964c67fc7fc8b310eb383d4
админ <admin@DESKTOP-VAO6CRN>
2021-05-22 00:57:03 +0300

    1

---
+++ Client/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static string ip = "127.0.0.1";
+        static int port = 8080;
+
+        static void Main(string[] args)
+        {
+
+            Task.Run(() => Test()).Wait();
+        }
+
+        public static void Test()
+        {
+
+            try
+            {
+
+                EndPoint endPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                
+                
+                socket.Connect(endPoint);
+               
+                int size;
+
+                while (true) {
+                    Console.WriteLine("ВВедите сообщение");
+                    string message = Console.ReadLine();
+                    var data = Encoding.UTF8.GetBytes(message);
+                    socket.Send(data);
+                    byte[] buffer = new byte[256];
+                    
+                    var answer = new StringBuilder();
+                    do
+                    {
+                        size = socket.Receive(buffer);
+                        answer.Append(Encoding.UTF8.GetString(buffer, 0, size));
+                    }
+                    while (socket.Available > 0);
+
+                    Console.WriteLine(answer);
+                }
+
+            }
+
+
+
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+
+
+
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        static string ip = "127.0.0.1";
+        static int port = 8080;
+
+
+
+        static void Main(string[] args)
+        {
+            Task.Run(() => Test()).Wait();
+
+        }
+
+        public static void Test()
+        {
+            
+                EndPoint endPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            socket.Bind(endPoint);
+            socket.Listen(5);
+            var listener = socket.Accept();
+            byte[] buffer = new byte[256];
+            int size = 0;
+            
+            while (true)
+            {
+                var data = new StringBuilder();
+                do
+                {
+                    size = listener.Receive(buffer);
+                    data.Append(Encoding.UTF8.GetString(buffer, 0, size));
+                }
+                while (listener.Available > 0);
+                Console.WriteLine(data);
+                listener.Send(Encoding.UTF8.GetBytes("успех"));
+
+            }
+
+
+        }
+    }
+}
