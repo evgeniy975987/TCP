5eb651f9a32361294abeee416c8717c0dc89847d
админ <admin@DESKTOP-VAO6CRN>
2021-05-29 23:14:05 +0300

    добавил возможность передавать данные

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,20 +1,31 @@
 ﻿using System;
+using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Client
 {
     class Program
     {
+
         static string ip = "127.0.0.1";
         static int port = 8080;
-
         static void Main(string[] args)
         {
 
-            Task.Run(() => Test()).Wait();
+            //Task.Run(() => Test()).Wait();
+            try
+            {
+                Task.Run(() => Test2()).Wait();
+                Console.ReadLine();
+            }
+            catch (Exception ex) {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
         }
 
         public static void Test()
@@ -36,7 +47,7 @@ namespace Client
                     string message = Console.ReadLine();
                     var data = Encoding.UTF8.GetBytes(message);
                     socket.Send(data);
-                    byte[] buffer = new byte[256];
+                    byte[] buffer = new byte[32];
                     
                     var answer = new StringBuilder();
                     do
@@ -57,6 +68,88 @@ namespace Client
 
 
 
+        }
+
+        public static void Test2() {
+
+           
+            
+            int portClient = 8080;
+
+
+            
+            string IPserver = "192.168.1.72";
+            int portServer = 8010;
+
+            IPEndPoint ReceiveEndPoint = new IPEndPoint(IPAddress.Parse(IPserver), portServer);
+
+            
+
+            UdpClient udpSender = new UdpClient( portClient);
+          
+            
+
+            string path = @"C:\Users\админ\Desktop\файлы для тестов программ\1.JPG";
+            int packetSize = 8192;
+            byte[] packetSend;
+            byte[] packetRecive;
+
+            try
+            {
+
+                using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read)) {
+                    udpSender.Client.ReceiveTimeout = 10000;
+                    
+                    int numBytesRead = 0;
+                    int BytesToRead = (int)fs.Length;
+
+                    String fileName = Path.GetFileName(path);
+                    String format = Path.GetExtension(path);
+                    packetSend = Encoding.Unicode.GetBytes(fileName);
+                    
+                    udpSender.Send(packetSend, packetSend.Length, IPserver, portServer);
+
+
+                    packetRecive = udpSender.Receive(ref ReceiveEndPoint);
+                    
+                    int parts = (int) fs.Length / packetSize;
+
+                    if ((int)fs.Length % packetSize != 0) parts++;
+
+                    packetSend = BitConverter.GetBytes(parts);
+
+                    udpSender.Send(packetSend, packetSend.Length, IPserver, portServer);
+
+                    packetRecive = udpSender.Receive(ref ReceiveEndPoint);
+
+                    packetSend = new byte[packetSize];
+                    int n = 0;
+                    
+                    
+                    for (int i = 0; i < parts - 1; i++) {
+                        n = fs.Read(packetSend, 0, packetSize);
+                        if (n == 0) break;
+                        numBytesRead += n;
+                        BytesToRead -= n;
+                        
+                        udpSender.Send(packetSend, packetSend.Length, IPserver, portServer);
+                        packetRecive = udpSender.Receive(ref ReceiveEndPoint);
+                        Console.WriteLine(i);
+                        Thread.Sleep(5);
+                    }
+                    packetSend = new byte[BytesToRead];
+                    n = fs.Read(packetSend, 0, BytesToRead);
+                    udpSender.Send(packetSend, packetSend.Length, IPserver, portServer);
+                    packetRecive = udpSender.Receive(ref ReceiveEndPoint);
+                }
+                Console.WriteLine("файл отправлен");
+            }
+
+            catch (Exception ex) {
+
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
         }
     }
 }
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
@@ -15,22 +16,23 @@ namespace Server
 
         static void Main(string[] args)
         {
-            Task.Run(() => Test()).Wait();
+            //Task.Run(() => Messenger()).Wait();
 
+            Test2();
         }
 
-        public static void Test()
+        public static void Messenger()
         {
-            
-                EndPoint endPoint = new IPEndPoint(IPAddress.Parse(ip), port);
-                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            EndPoint endPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 
             socket.Bind(endPoint);
             socket.Listen(5);
             var listener = socket.Accept();
-            byte[] buffer = new byte[256];
+            byte[] buffer = new byte[64];
             int size = 0;
-            
+
             while (true)
             {
                 var data = new StringBuilder();
@@ -45,6 +47,50 @@ namespace Server
 
             }
 
+
+        }
+
+        public static void Test2()
+        {
+
+
+            string ipClient = "192.168.1.70";
+            //string ipClient = "127.0.0.1";
+            int portClient = 8080;
+
+
+            //string IPserver = "192.168.1.70";
+            string IPserver = "192.168.1.72";
+            int portServer = 8010;
+
+
+            IPEndPoint recieveEndPoint = new IPEndPoint(IPAddress.Parse(ipClient), portClient);
+            UdpClient udpReciever = new UdpClient(portServer);
+            byte[] packetRecieve = new byte[1];
+            while (true)
+            {
+
+                byte[] packetSend = new byte[1];
+                packetRecieve = udpReciever.Receive(ref recieveEndPoint);
+                string name = Encoding.Unicode.GetString(packetRecieve);
+                udpReciever.Send(packetSend, 1, ipClient,portClient);
+                packetRecieve = udpReciever.Receive(ref recieveEndPoint);
+                int parts = BitConverter.ToInt32(packetRecieve, 0);
+                using (FileStream fs = new FileStream($"{name}.JPG", FileMode.Create, FileAccess.Write))
+                {
+
+                    for (int i = 0; i < parts; i++)
+                    {
+                        udpReciever.Send(packetSend, 1, ipClient, portClient);
+                        packetRecieve = udpReciever.Receive(ref recieveEndPoint);
+                        fs.Write(packetRecieve, 0, packetRecieve.Length);
+                        udpReciever.Send(new byte[1], 1, ipClient,portClient);
+                        Console.WriteLine(i);
+                    }
+
+                    Console.WriteLine("файл получен");
+                }
+            }
 
         }
     }
